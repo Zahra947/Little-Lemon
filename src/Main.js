43c347ab4752cc63b68b54959a661a3e191d5a34
Zahra947 <85116470+Zahra947@users.noreply.github.com>
2023-01-13 19:29:25 +0330

    1 binaries uploaded

--- src/Main.js
+++ src/Main.js
@@ -3,8 +3,23 @@ import React from 'react';
 export default function Main() {
   return (
     <main className="main">
-      <section>section one</section>
-      <section>section 2</section>
+      <section>
+        <article>
+          <h2>Little Lemon</h2>
+          <h1>Chicago</h1>
+          <br />
+          <p>
+            We are a family owned a Mideterranean restaurant, focudes on
+            traditional recipes served with a modern twist.
+          </p>
+          <button>Resserve a Table</button>
+        </article>
+        <img src="" alt="" />
+      </section>
+      <section>
+        <article></article>
+        <table></table>
+      </section>
     </main>
   );
 }
