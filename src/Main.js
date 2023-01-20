6d4cf94b7261afac90ee7cee4978dae619fbe76c
Zahra947 <85116470+Zahra947@users.noreply.github.com>
2023-01-20 19:44:48 +0330

    3 files modified

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import DeliveryDiningOutlinedIcon from '@mui/icons-material/DeliveryDiningOutlined';
 export default function Main() {
   return (
     <main className="main">
@@ -14,24 +14,56 @@ export default function Main() {
           </p>
           <button>Resserve a Table</button>
         </article>
-        <img src="./media/reserve.png" alt="reserve" className="m_section1_image" />
+        <img
+          src="./media/reserve.png"
+          alt="reserve"
+          className="m_section1_image"
+        />
       </section>
       <section className="M_section2">
         <article className="M_section2_a">This Week Specials!</article>
         <button className="M_section2_b">Order Online</button>
       </section>
       <div className="M_section3">
-        <section>
+        <section className="foods">
           <img src="./media/greek-salad" alt="Greek Salad" />
-          <article>Greek Salad</article>
+          <h3 className="foodName">
+            Greek Salad &nbsp;<span className="priceName">$ 12.99</span>
+          </h3>
+          <article className="M_section3_a">
+            The famous greek salad of crispy lettuse, peppers, olives and our
+            Chicago style feta cheese, garnished with crunchy garlic and
+            rosemary croutons.
+          </article>
+          <button>
+            <DeliveryDiningOutlinedIcon />
+          </button>
         </section>
-        <section>
+        <section className="foods">
           <img src="./media/bruchetta.jpg" alt="Bruchetta" />
-          <article>Bruchetta</article>
+          <h3 className="foodName">
+            Bruchetta &nbsp;<span className="priceName">$ 5.99</span>
+          </h3>
+          <article className="M_section3_a">
+            Our Bruchetta is made from grilled bread that has been smeared with
+            garlic and seasoned with salt and olive oil.
+          </article>
+          <button>
+            <DeliveryDiningOutlinedIcon />
+          </button>
         </section>
-        <section>
+        <section className="foods">
           <img src="./media/lemon-desert" alt="Lemon Desert" />
-          <article>Lemon Desert</article>
+          <h3 className="foodName">
+            Lemon Desert &nbsp;<span className="priceName">$ 5.00</span>
+          </h3>
+          <article className="M_section3_a">
+            This comes straight from grandma's recipe book, every lsat ingrdient
+            has been sourced and is as authentic as can be imagined.
+          </article>
+          <button>
+            <DeliveryDiningOutlinedIcon />
+          </button>
         </section>
       </div>
     </main>
